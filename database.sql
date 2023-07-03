180417eac6b9fd246fd38629e7fe69a1951a0ef7
john <tamjohn@student.ubc.ca>
2023-07-03 05:08:58 -0700

    created GET and PUT APIs for bookings

--- database.sql
+++ database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE eps2669;
+
 CREATE TABLE BOOKINGS (
     eid INTEGER PRIMARY KEY,
     title VARCHAR (100) NOT NULL,
@@ -12,3 +14,6 @@ VALUES (1, 'A303 Badminton', '2023-07-02 14:30:00', '2023-07-02 15:30:00');
 
 INSERT INTO BOOKINGS (eid, title, start_time, end_time)
 VALUES (2, 'D123 Basketball', '2023-07-04 18:30:00', '2023-07-04 20:30:00');
+
+INSERT INTO BOOKINGS (eid, title, start_time, end_time)
+VALUES (3, 'B004 Basketball', '2023-07-07 18:30:00', '2023-07-07 20:30:00');
--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 //ROUTES//
 
 // create a booking
-
 app.post("/bookings", async (req, res) => {
     try {
         const { eid, title, start_time, end_time, allDay, resource } = req.body;
@@ -18,7 +17,6 @@ app.post("/bookings", async (req, res) => {
         const valuePlaceholders = ["$1", "$2", "$3", "$4"];
         const insertColumns = ["eid", "title", "start_time", "end_time"];
 
-        // Add optional columns if provided
         if (allDay !== undefined) {
             insertColumns.push("allDay");
             insertValues.push(allDay);
@@ -30,10 +28,8 @@ app.post("/bookings", async (req, res) => {
             valuePlaceholders.push(`$${insertValues.length}`);
         }
 
-        // Build the query string
         const query = `INSERT INTO bookings (${insertColumns.join(", ")}) VALUES (${valuePlaceholders.join(", ")}) RETURNING *`;
 
-        // Execute the query
         const newBooking = await pool.query(query, insertValues);
 
         res.json(newBooking.rows[0]);
