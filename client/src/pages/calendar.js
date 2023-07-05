89f50da960c3b4242799fe2019dcfb834e744b1c
tamjohn <tamjohnathan@gmail.com>
2023-07-05 02:33:29 -0700

    fixed Date error when displaying event in weekly view

--- client/src/pages/calendar.js
+++ client/src/pages/calendar.js
@@ -7,7 +7,6 @@ import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { zhTW } from "date-fns/locale";
 import { useBookings } from "../hooks/useBookings";
 
 const locales = {
@@ -26,35 +25,48 @@ let formats = {
     timeGutterFormat: 'HH:mm'
 }
 
-const testDateTime = "2023-07-02T15:30:00.000Z";
+// const testDateTime = "2023-07-02T15:30:00.000Z";
 
-function removeTimeZone(datetime) {
-    return datetime.slice(0, -5);
-}
+// function removeTimeZone(datetime) {
+//     return datetime.slice(0, -5);
+// }
 
-const events = [
-    {
-        id: '2',
-        title: "A303 Basketball",
-        start: new Date(removeTimeZone(testDateTime)),
-        end: new Date("2023-07-02T19:30:00"),
-    },
-    {
-        title: "Vacation",
-        start: new Date("2023-07-05T15:30:00.000Z"),
-        end: new Date("2023-07-05T19:30:00.000Z"),
-    },
-    {
-        title: "Conference",
-        start: new Date(2023, 7, 20),
-        end: new Date(2023, 7, 23),
-    },
-];
+// const events = [
+//     {
+//         id: '2',
+//         title: "A303 Basketball",
+//         start: new Date(removeTimeZone(testDateTime)),
+//         end: new Date("2023-07-02T19:30:00"),
+//     },
+//     {
+//         title: "Vacation",
+//         start: new Date("2023-07-05T15:30:00"),
+//         end: new Date("2023-07-05T19:30:00"),
+//     },
+//     {
+//         title: "Conference",
+//         start: new Date(2023, 7, 20),
+//         end: new Date(2023, 7, 23),
+//     },
+// ];
 
 function CalendarTemplate() {
     const hook = useBookings();
+    const bookingsData = hook.hookBookings;
 
-    console.log('booking data', hook.hookBookings);
+    function convertDate(data) {
+        const modifiedData = data.map(item => {
+            if (item.start) {
+                item.start = new Date(item.start)
+            }
+            if (item.end) {
+                item.end = new Date(item.end)
+            }
+            return item;
+        });
+
+        return modifiedData;
+    }
 
     return (
         <div>
@@ -62,7 +74,7 @@ function CalendarTemplate() {
             <Calendar
                 localizer={localizer}
                 formats={formats}
-                events={hook.hookBookings}
+                events={convertDate(bookingsData)}
                 startAccessor="start"
                 endAccessor="end"
                 style={{ height: 900, margin: "50px" }}
